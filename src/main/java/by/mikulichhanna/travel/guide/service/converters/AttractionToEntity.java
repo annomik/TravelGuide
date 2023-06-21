1d68e6a0884815eb659e036e9e02f944cf633d7b
Hanna Mikulich <mikulichhanna@gmail.com>
2023-06-21 23:37:44 +0300

    added converter

---
+++ src/main/java/by/mikulichhanna/travel/guide/service/converters/AttractionToEntity.java
@@ -0,0 +1,28 @@
+package by.mikulichhanna.travel.guide.service.converters;
+
+import by.mikulichhanna.travel.guide.core.dto.AttractionDTO;
+import by.mikulichhanna.travel.guide.entity.AttractionEntity;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Component
+public class AttractionToEntity implements Converter<AttractionDTO, AttractionEntity>{
+
+        @Override
+        public AttractionEntity convert(AttractionDTO attractionDTO) {
+            LocalDateTime dtCreate = LocalDateTime.now().withNano(3);
+
+            return new AttractionEntity(UUID.randomUUID(),
+                    dtCreate,
+                    dtCreate,
+                    attractionDTO.getName(),
+                    attractionDTO.getAddress()
+                    );
+        }
+
+
+
+}
